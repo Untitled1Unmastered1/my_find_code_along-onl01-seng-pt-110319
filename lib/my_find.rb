1dd2db97c7af7e49932cc658fd49d7e4dab8227c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-18 05:44:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,7 @@
-require 'pry'
-
 def my_find(collection)
-  i = 0 
-  while i < collection.length 
-   return collection[i] if yield(collection[i])
- end 
-  i = i + 1 
-end 
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
